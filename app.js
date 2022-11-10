aa34675c4832d182187b2697b64edf0f4ed22b1c
goleccc <113783833+golecl@users.noreply.github.com>
2022-11-10 14:05:13 +0000

    code :)

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
+
+const octokit = new Octokit({ 
+    auth: 'YOUR-TOKEN',
+  });
+
+await octokit.request("GET /octocat", {});
+
+const grapher = document.querySelector('.req');
